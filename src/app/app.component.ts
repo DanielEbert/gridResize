c5496293e5961528594869781e5fe1bd3c00eed2
Daniel Ebert <iuq@web.de>
2024-11-23 16:34:01 +0100

    add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { TileComponent } from './tile/tile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [MatGridListModule, CommonModule],
+  imports: [MatGridListModule, CommonModule, TileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -15,8 +16,10 @@ export class AppComponent {
     { text: 'Tile 3', colspan: 1, rowspan: 1 },
   ];
 
-  private numCols = 3;
-  private colWidth = 0;
+  protected numCols = 3;
+  protected rowHeight = 150;
+
+  protected colWidth = 0;
   private resizingTile: any = null;
   private startX: number = 0;
   private startY: number = 0;
@@ -44,14 +47,17 @@ export class AppComponent {
     const deltaY = event.clientY - this.startY;
 
     const colChange = Math.round(deltaX / this.colWidth);
-    const rowChange = Math.round(deltaY / 200);
+    const rowChange = Math.round(deltaY / this.rowHeight);
 
     this.startX += colChange * this.colWidth;
-    this.startY += rowChange * 200;
+    this.startY += rowChange * this.rowHeight;
 
     this.resizingTile.colspan += colChange;
     this.resizingTile.rowspan += rowChange;
-    if (this.resizingTile.colspan < 1) this.resizingTile.colspan = 1;
+    this.resizingTile.colspan = Math.max(
+      1,
+      Math.min(this.resizingTile.colspan, this.numCols)
+    );
     if (this.resizingTile.rowspan < 1) this.resizingTile.rowspan = 1;
   }
 
